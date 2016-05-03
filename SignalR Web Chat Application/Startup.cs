d58a77db3db9d6f168a32cd9052b37f4b5e0a7f8
Shaun Grech <shaun.grech13@gmail.com>
2016-05-03 10:28:02 +0200

    Initial Commit

---
+++ SignalR Web Chat Application/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalR_Web_Chat_Application.Startup))]
+namespace SignalR_Web_Chat_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
